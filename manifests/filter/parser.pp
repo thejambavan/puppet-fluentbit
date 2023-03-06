5ed360a0a0a2257b78346ff123bb84fd4aac6010
Seth Tunstall <seth@tunstall.in>
2023-03-06 12:40:04 +0000

    add support for parser filter plugin

---
+++ manifests/filter/parser.pp
@@ -0,0 +1,21 @@
+# @summary Filter to parser records using rules and conditions
+#
+# @param configfile
+#  Path to the filter configfile. Naming should be filter_*.conf to make sure
+#  it's getting included by the main config.
+define fluentbit::filter::parser (
+  String $configfile         = '/etc/fluent-bit/filter_parser.conf',
+  String $match              = '*',
+  String $key_name           = 'log',
+  String $parser             = 'my_parser,
+) {
+  # create filter_parser.conf
+  # TODO: concat for multiple entries
+  file { $configfile:
+    ensure  => file,
+    mode    => '0644',
+    content => template('fluentbit/filter/parser.conf.erb'),
+    notify  => Class['fluentbit::service'],
+    require => Class['fluentbit::install'],
+  }
+}
