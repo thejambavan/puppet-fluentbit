e8af9c62787d782512e8d4bbec589dfd9e50b408
Seth Tunstall <seth@tunstall.in>
2023-03-06 16:21:26 +0000

    multiline WIP

---
+++ manifests/input/tail_multiline.pp
@@ -0,0 +1,21 @@
+# @summary Input plugin to monitor one or several text files. Split off from the main 'tail' input
+# because multiline is handled differently for fluent-bit  >1.8 
+
+define fluentbit::input::tail (
+  Stdlib::Absolutepath $path,
+  Stdlib::Absolutepath $configfile = "/etc/fluent-bit/input_tail_${name}.conf",
+  String $tail_multiline_parser,
+  Boolean $read_from_head          =  true,
+  String $db                       = "/var/log/fluent-bit.db"
+
+) {
+  $skip_long_lines_string = bool2str($skip_long_lines, 'On', 'Off')
+  $docker_mode_string = bool2str($docker_mode, 'On', 'Off')
+
+  file { $configfile:
+    ensure  => file,
+    mode    => $fluentbit::config_file_mode,
+    content => template('fluentbit/input/tail_multiline.conf.erb'),
+    notify  => Class['fluentbit::service'],
+  }
+}
